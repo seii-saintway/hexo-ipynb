19fb603ad40c1d46fad778122e27b55e5deb5a8f
saintway @ Andrews-MacBook-Pro <saintway.sakaru@gmail.com>
2021-10-01 12:16:34 +0900

    update nbconvert to 6.2.0

--- convert.py
+++ convert.py
@@ -1,22 +1,27 @@
-from __future__ import print_function
-import sys
 import os
-from nbconvert import HTMLExporter
+import sys
+
+from traitlets.config import Config
+from nbconvert.exporters.html import HTMLExporter
 
 class MyExporter(HTMLExporter):
-    @property
-    def template_path(self):
-        """
-        We want to inherit from HTML template, and have template under
-        `./templates/` so append it to the search path. (see next section)
-        """
-        return super().template_path + [os.path.dirname(__file__)]
+    def __init__(self):
+        super().__init__()
+        self.template_paths = super().template_paths + [os.path.dirname(__file__)]
+
+#     @property
+#     def template_paths(self):
+#         """
+#         We want to inherit from HTML template, and have template under `./`
+#         so append it to the search path.
+#         """
+#         return super().template_paths + [os.path.dirname(__file__)]
 
     def _template_file_default(self):
         """
         We want to use the new template we ship with our library.
         """
-        return 'hexo'
+        return 'hexo.tpl'
 
 def main(ipynb_file):
     exporter = MyExporter()
